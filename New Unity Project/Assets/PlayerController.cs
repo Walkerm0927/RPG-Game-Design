75e25ae9d678c5f77b9526f2bd7080aab49f5676
Phil Solimine <ps12e@my.fsu.edu>
2019-10-01 11:55:15 -0400

    Changed movement script name

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class script : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rigidbody;
